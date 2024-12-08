27adf99fcbac9a47939568446e36ec81c35ee5dd
nc-jodie-cheung <jodie.cheung@northcoders.com>
2024-12-08 19:06:19 +0000

    supabase upload image

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,10 +1,27 @@
 "use server";
 
-import { profileSchema } from "./schemas";
+import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+
+//we want the whole user for the profile
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
 
 export const createProfileAction = async (
   prevState: any,
@@ -12,12 +29,10 @@ export const createProfileAction = async (
 ) => {
   try {
     const user = await currentUser();
-    // console.log(user, "USER");
-
     if (!user) throw new Error("Please login to create a profile");
 
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.create({
       data: {
@@ -33,10 +48,73 @@ export const createProfileAction = async (
       },
     });
   } catch (error) {
-    return {
-      message: error instanceof Error ? error.message : "An error occurred",
-    };
+    return renderError(error);
   }
   redirect("/");
 };
 //if you are able to create a profile, you want to go to the home page
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
+};
+
+//write another function to get the user profile
+//we wrap the clerk in the get user
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+//repeat the same steps as create profile
+//use safe parse option - were not going to throw error
+//results are handled ourselves
+//what we get back isn't going to be the same
+//the result will be object with values and success property that is true or false
+//if the success is false, we want to handle that error
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+    revalidatePath("/profile");
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const updateProfileImageAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const image = formData.get("image") as File;
+  const validateFields = validateWithZodSchema(imageSchema, { image });
+  console.log(validateFields, "VALIDATE");
+
+  return { message: "Profile image updated successfully" };
+};
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,8 +2,48 @@ import * as z from "zod";
 import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
-  // firstName: z.string().max(5, { message: 'max length is 5' }),
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  username: z.string().min(2),
+  firstName: z.string().min(2, {
+    message: "first name must be at least 2 characters",
+  }),
+  lastName: z.string().min(2, {
+    message: "last name must be at least 2 characters",
+  }),
+  username: z.string().min(2, {
+    message: "user name must be at least 2 characters",
+  }),
 });
+
+//generic type using T
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+    throw new Error(errors.join(","));
+  }
+  return result.data;
+}
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+function validateFile() {
+  //check whether image is a file
+  const maxUploadSize = 1024 * 1024;
+  const acceptedFileTypes = ["image/"];
+  return z
+    .instanceof(File)
+    .refine((file) => {
+      return !file || file.size <= maxUploadSize;
+    }, "File size must be less than 1 MB")
+    .refine((file) => {
+      return (
+        !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
+      );
+    }, "File must be an image");
+}
+//it needs to be a file
+//checking for the type
